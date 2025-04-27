819f28b04b1bb15694ee65e5855a5c0e85fd3c93
sjoshuam <sjoshuam@github.com>
2025-04-27 14:04:14 -0400

    Separate generic unix setup from docker/podman specific set up

--- 10_make_services/11_docker_server.sh
+++ 10_make_services/11_docker_server.sh
@@ -3,58 +3,6 @@
 echo -e "\nWARNING: This script has been tested piece by piece,
 but not all the way through. Execute with caution.\n"
 
-## TIMEZONE: set the timezone
-timedatectl set-timezone America/New_York
-
-## UPGRADE: apply all updates for a non-mac unix system
-if [ ! $(uname) = "Darwin" ]; then
-  apt-get update
-  apt-get dist-upgrade -y
-  apt-get autoremove -y
-  apt-get autoclean -y
-fi
-
-
-## SSH SERVER: set up ssh server for a non-mac unix system
-if [ ! $(uname) = "Darwin" ]; then
-  apt-get install openssh-server -y
-  systemctl start ssh
-  systemctl enable ssh
-  echo "SSH login:" $(whoami)"@"$(hostname -I)
-  ## systemctl stop ssh
-fi
-
-    #### notes
-    ## mac: system preferences > sharing > remote login
-    ## Check server status: systemctl status ssh
-    ## Shut down server: systemctl stop ssh
-
-
-# SSH KEYS: set up ssh keys
-if [ ! -d "~/.ssh" ]; then mkdir ~/.ssh; fi
-ssh-keygen -t ed25519 -C $(hostname) -f ~/.ssh/id_ed25519
-echo "== ADD THIS KEY TO GITHUB ========"
-cat ~/.ssh/id_ed25519.pub
-
-    #### notes
-    ## ssh-keygen -t rsa -b 4096 -C $(hostname) -f ~/.ssh/ed25519
-
-
-## BASHRC:  modify the bashrc file
-if [ ! -f "~.bashrc_backup" ]; then cp ~/.bashrc ~/.bashrc_backup; fi
-cp ~/.bashrc_backup ~/.bashrc
-
-
-## UTILITIES: install various useful software
-apt-get install ca-certificates curl -y # web untilities
-
-    #### notes
-    ## basic loop: for iter in a b c; do echo iter; done
-
-
-## GIT
-if [ ! -d "~/code" ]; then mkdir ~/code; fi
-
 
 ## DOCKER: install docker
 curl -fsSL https://get.docker.com -o install-docker.sh
@@ -62,14 +10,17 @@ sh install-docker.sh && rm install-docker.sh
 apt-get install -y uidmap
 dockerd-rootless-setuptool.sh install
 
+
   ## add variables to bashrc
 echo -e "\n# Docker Variables" >> ~/.bashrc
 echo "export PATH=/usr/bin:$PATH" >> ~/.bashrc
 echo "export DOCKER_HOST=unix:///run/user/$(id -u).docker.sock" >> ~/.bashrc
 
+
   ## start the server in rootless mode
 systemctl --user start docker
 systemctl --user enable docker
+
 
   ## download useful images
 docker pull ubuntu:24.04 ## operating system
@@ -85,7 +36,4 @@ docker pull elasticsearch:8.17.4 ## search engine
     ## stop docker: systemctl --user stop docker
     ## elasticsearch: docker pull elasticsearch:8.17 ## search engine
 
-  ## CONTAINERS: install docker containers
-
-  docker build -t 12_devpi.docker
   
--- 10_make_services/11_unix_setup.sh
+++ 10_make_services/11_unix_setup.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 ## TIMEZONE: set the timezone
 timedatectl set-timezone America/New_York
 
@@ -9,17 +11,39 @@ apt-get autoremove -y
 apt-get autoclean -y
 
 
+## generate ssh keys
+if [ ! -d "$HOME/.ssh" ]; then mkdir $HOME/.ssh; fi
+ssh-keygen -t ed25519 -C $(hostname) -f $HOME/.ssh/id_ed25519
+
+    #### notes
+    ## get public key: cat $HOME/.ssh/id_ed25519.pub
+    ## make an rsa key: ssh-keygen -t rsa -b 4096 -C $(hostname) -f $HOME/.ssh/id_sa
+
+
 ## SSH SERVER: set up ssh server
 apt-get install openssh-server -y
 systemctl start ssh
 systemctl enable ssh
-echo "SSH login:" $(whoami)"@"$(hostname -I)
+
+    #### notes
+    ## mac: system preferences > sharing > remote login
+    ## Check server status: systemctl status ssh
+    ## Shut down server: systemctl stop ssh
 
 
 ## BASHRC:  modify the bashrc file
-if [ ! -f "~.bashrc_backup" ]; then cp ~/.bashrc ~/.bashrc_backup; fi
-cp ~/.bashrc_backup ~/.bashrc
+if [ ! -f "$HOME/.bashrc_backup" ]; then echo "Backing up bashrc" && cp $HOME/.bashrc $HOME/.bashrc_backup; fi
+cp $HOME/.bashrc_backup $HOME/.bashrc
 
 
 ## UTILITIES: install various useful software
 apt-get install ca-certificates curl -y # web untilities
+
+
+## DIRECTORIES: create a standard directory for code
+if [ ! -d "$HOME/code" ]; then echo "Making code directory" mkdir $HOME/code; fi
+
+
+### REMINDERS: Misc.
+    ## basic loop: for iter in a b c; do echo iter; done
+    ## get ssh login: echo "SSH login:"ssh $(whoami)"@"$(hostname -I)
