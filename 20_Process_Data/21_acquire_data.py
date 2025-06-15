70fbf1c1a59a80f1fd0d2988bbc1a94dcbcec0ec
sjoshuam <sjoshuam@github.com>
2025-06-15 18:14:24 -0400

    Retrieve data from IRS website

--- 20_Process_Data/21_acquire_data.py
+++ 20_Process_Data/21_acquire_data.py
@@ -12,7 +12,11 @@ be refreshed with future years.
 ## Note: environment created with python3 -m venv venv_20; source .venv_20/bin/activate
 
 ## load libraries
-from pyspark.sql import SparkSession
+import pandas as pd
+from warnings import warn
+from typing import Union
+import requests, os
+
 
 ## load setting
 param = dict()
@@ -24,12 +28,8 @@ param = dict()
 class MoveData:
     '''TODO'''
 
-    def __init__(self, inflow:dict(), outflow:dict()):
-        self.year = set(list(inflow.keys()) + list(outflow.keys()))
-        self.inflow = inflow
-        self.outflow = outflow
-        assert inflow.keys() == outflow.keys(), "inflow/outflow data years don't match"
-        self.year = set(inflow.keys())
+    def __init__(self):
+        pass
 
 
 
@@ -40,6 +40,62 @@ class MoveData:
 
 
 ########## PULL COUNTY-TO-COUNTY MIGRATION DATA FROM IRS
+
+def get_irs_movement_data():
+    """Download csv files from irs"""
+
+    ## create folders if they do not exist
+    for iter in ['input', 'output']:
+        if not os.path.isdir(iter):
+            warn(f"Created new directory: {iter}")
+            os.mkdir(iter)
+
+    ## construct roster of files
+    file_roster: pd.DataFrame = pd.DataFrame({'year':range(2011, pd.Timestamp.now().year)})
+    file_roster['status'] = 'unknown'
+    file_roster['file'] = file_roster.apply(
+        lambda row: f"countyinflow{row['year']-2000}{row['year']-1999}.csv", axis= 1)
+    file_roster['url'] = file_roster.apply(
+        lambda row: f"https://www.irs.gov/pub/irs-soi/{row['file']}", axis= 1)
+    file_roster= file_roster.set_index('year')
+
+    ## download files if they do not already exist
+    for iter in file_roster.index:
+        if not os.path.isfile(path= 'input/'+file_roster['file'].at[iter]):
+            file_roster['status'].at[iter] = 'Not found'
+            retrieved_data = requests.get(url= file_roster['url'].at[iter])
+            if retrieved_data.status_code == 200:
+                file_roster['status'].at[iter] = 'Downloaded now'
+                open('input/'+file_roster['file'].at[iter], 'w').write(retrieved_data.text)
+        else: 
+            file_roster['status'].at[iter] = 'Downloaded before'
+
+    ## communicate outcomes from file retrieval
+    assert not ('unknown' in set(file_roster.status)), "Some file retrievals skipped"
+    print('==== File retrieval outcomes ================')
+    print(file_roster['status'].value_counts().sort_index())
+
+    ## define function to read each file and extract
+    def process_move_data(file: str) -> pd.DataFrame:
+        """Extract useful information from a csv file"""
+        cols= {'y1_statefips':str, 'y1_countyfips':str, 'y2_statefips':str}
+        cols.update({'y2_countyfips':str, 'n2':int, 'agi':int})
+        move_data= pd.read_csv(file, usecols= cols.keys(), dtype= cols)
+        move_data= move_data.loc[move_data['y1_statefips'].astype(int)< 57]
+        move_data= move_data.rename(
+            {'n2':'people', 'y2_statefips':'dest', 'y1_statefips':'origin'}, axis= 1)
+        move_data['dest']= move_data['dest'] + move_data['y2_countyfips']
+        move_data['origin']= move_data['origin'] + move_data['y1_countyfips']
+        move_data = move_data.drop(['y1_countyfips', 'y2_countyfips'], axis= 1)
+        return move_data
+    
+
+    move_data= process_move_data('input/'+file_roster['file'].at[2011])
+    print(move_data)
+
+    ## execute data extraction function in parallel on all files
+
+    ## construct MoveData class object
 
 
 ########## PULL COUNTY CHARACTERISTICS DATA FROM BEA (OR CENSUS IF THAT'S BETTER)
@@ -53,14 +109,9 @@ class MoveData:
 if __name__ == '__main__':
 
     ## create a spark session
-    spark = SparkSession.builder.appName("acquire_data")
-    move_data = MoveData(
-        inflow = {2022:1, 2021:2},
-        outflow = {2021:1, 2022:2}
-        )
+    x = get_irs_movement_data()
 
     ## pull in county to county migration data from IRS
-    print(move_data.year)
     pass
 
 
