82b30bb859247714d9e058d1f9c2f9f6bf7d76d3
sjoshuam <sjoshuam@github.com>
2025-06-16 21:23:43 -0400

    Simplify and parallelize irs data processing

--- 20_Process_Data/21_acquire_data.py
+++ 20_Process_Data/21_acquire_data.py
@@ -13,9 +13,7 @@ be refreshed with future years.
 
 ## load libraries
 import pandas as pd
-from warnings import warn
-from typing import Union
-import requests, os
+import warnings, requests, os, multiprocessing
 
 
 ## load setting
@@ -41,78 +39,90 @@ class MoveData:
 
 ########## PULL COUNTY-TO-COUNTY MIGRATION DATA FROM IRS
 
-def get_irs_movement_data():
-    """Download csv files from irs"""
+def download_irs_data():
+    """Download csv files from irs, but not if they have already been downloaded"""
 
     ## create folders if they do not exist
     for iter in ['input', 'output']:
         if not os.path.isdir(iter):
-            warn(f"Created new directory: {iter}")
+            warnings.warn(f"Created new directory: {iter}")
             os.mkdir(iter)
 
     ## construct roster of files
-    file_roster: pd.DataFrame = pd.DataFrame({'year':range(2011, pd.Timestamp.now().year)})
-    file_roster['status'] = 'unknown'
-    file_roster['file'] = file_roster.apply(
+    data_inventory: pd.DataFrame = pd.DataFrame({'year':range(2011, pd.Timestamp.now().year)})
+    data_inventory['status'] = 'unknown'
+    data_inventory['file'] = data_inventory.apply(
         lambda row: f"countyinflow{row['year']-2000}{row['year']-1999}.csv", axis= 1)
-    file_roster['url'] = file_roster.apply(
+    data_inventory['url'] = data_inventory.apply(
         lambda row: f"https://www.irs.gov/pub/irs-soi/{row['file']}", axis= 1)
-    file_roster= file_roster.set_index('year')
+    data_inventory= data_inventory.set_index('year')
 
     ## download files if they do not already exist
-    for iter in file_roster.index:
-        if not os.path.isfile(path= 'input/'+file_roster['file'].at[iter]):
-            file_roster['status'].at[iter] = 'Not found'
-            retrieved_data = requests.get(url= file_roster['url'].at[iter])
+    for iter in data_inventory.index:
+        if not os.path.isfile(path= 'input/'+data_inventory['file'].at[iter]):
+            data_inventory['status'].at[iter] = 'Not found'
+            retrieved_data = requests.get(url= data_inventory['url'].at[iter])
             if retrieved_data.status_code == 200:
-                file_roster['status'].at[iter] = 'Downloaded now'
-                open('input/'+file_roster['file'].at[iter], 'w').write(retrieved_data.text)
+                data_inventory['status'].at[iter] = 'Downloaded now'
+                open('input/'+data_inventory['file'].at[iter], 'w').write(retrieved_data.text)
         else: 
-            file_roster['status'].at[iter] = 'Downloaded before'
+            data_inventory['status'].at[iter] = 'Downloaded before'
 
     ## communicate outcomes from file retrieval
-    assert not ('unknown' in set(file_roster.status)), "Some file retrievals skipped"
+    assert not ('unknown' in set(data_inventory.status)), "Some file retrievals skipped"
     print('==== File retrieval outcomes ================')
-    print(file_roster['status'].value_counts().sort_index())
+    print(data_inventory['status'].value_counts().sort_index())
+    return data_inventory
 
-    ## define function to read each file and extract
-    def process_move_data(file: str) -> pd.DataFrame:
-        """Extract useful information from a csv file"""
-        cols= {'y1_statefips':str, 'y1_countyfips':str, 'y2_statefips':str}
-        cols.update({'y2_countyfips':str, 'n2':int, 'agi':int})
-        move_data= pd.read_csv(file, usecols= cols.keys(), dtype= cols)
-        move_data= move_data.loc[move_data['y1_statefips'].astype(int)< 57]
-        move_data= move_data.rename(
-            {'n2':'people', 'y2_statefips':'dest', 'y1_statefips':'origin'}, axis= 1)
-        move_data['dest']= move_data['dest'] + move_data['y2_countyfips']
-        move_data['origin']= move_data['origin'] + move_data['y1_countyfips']
-        move_data = move_data.drop(['y1_countyfips', 'y2_countyfips'], axis= 1)
-        return move_data
+
+########## REFINE IRS DATA
+
+def process_irs_data(file: str) -> pd.DataFrame:
+    """Extract useful information from a csv file"""
+
+    ## read in useful columns
+    cols= {'y1_statefips':str, 'y1_countyfips':str, 'y2_statefips':str}
+    cols.update({'y2_countyfips':str, 'n2':int, 'agi':int})
+    move_data= pd.read_csv(file, usecols= cols.keys(), dtype= cols)
+
+    ## filter out summary codes and improve column name interpretability
+    move_data= move_data.loc[move_data['y1_statefips'].astype(int)< 57]
+    move_data= move_data.rename(
+        {'n2':'people', 'y2_statefips':'dest', 'y1_statefips':'origin'}, axis= 1)
     
+    ## assemble full fips codes
+    move_data['dest']= move_data['dest'] + move_data['y2_countyfips']
+    move_data['origin']= move_data['origin'] + move_data['y1_countyfips']
+    move_data = move_data.drop(['y1_countyfips', 'y2_countyfips'], axis= 1)
+    return move_data
 
-    move_data= process_move_data('input/'+file_roster['file'].at[2011])
-    print(move_data)
+    #move_data= process_move_data('input/'+file_roster['file'].at[2011])
+    #print(move_data)
 
-    ## execute data extraction function in parallel on all files
 
-    ## construct MoveData class object
+########## DEFINE CLASS FOR IRS DATA (OR LEAVE AS A DATAFRAME?)
 
 
 ########## PULL COUNTY CHARACTERISTICS DATA FROM BEA (OR CENSUS IF THAT'S BETTER)
 
-
-########## MAKE CODE AUTOMATICALLY QUERY FOR FUTURE YEARS
 
 
 ########## TEST EXECUTION OF ALL CODE
 
 if __name__ == '__main__':
 
-    ## create a spark session
-    x = get_irs_movement_data()
+    ## initate a parallel processing pool
+    pool= multiprocessing.Pool(processes= min(max(os.cpu_count() // 2, 1), 8))
 
-    ## pull in county to county migration data from IRS
-    pass
+    ## download irs data (if not already downloaded)
+    irs_data_inventory= download_irs_data()
+
+    ## process and compile data
+    #irs_data= pool.map_async(
+    #    func= process_irs_data,
+    #    callback= lambda x:x, TODO: construct class
+    #    iterable= [1, 2, 3]#irs_data_inventory TODO:  extract a simple iterable
+    #)
 
 
 ##########==========##########==========##########==========##########==========##########==========
