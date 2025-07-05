e7e5a976b96af6ca39e676afc3cfd8486022e558
sjoshuam <sjoshuam@github.com>
2025-07-05 18:27:48 -0400

    Extract data from irs files and compile

--- 20_Process_Data/21_acquire_data.py
+++ 20_Process_Data/21_acquire_data.py
@@ -41,14 +41,16 @@ class MoveData:
 
 class IRS:
 
-    #### initialize empty class instance
     def __init__(self):
-        self.move_data = list()
+        """Initialize a class instance"""
+        self.data_from_file = dict()
+        self.valid_years = list()
+        self.move_data = pd.DataFrame()
         pass
+        
     
-    #### downloaded data files
-    def make_file_inventory(self):
-        """Download csv files from irs, but not if they have already been downloaded"""
+    def make_file_inventory(self, start_year=2011):
+        """Download data files from irs, but not if they have already been downloaded"""
 
         ## create folders if they do not exist
         for iter in ['input', 'output']:
@@ -57,12 +59,12 @@ class IRS:
                 os.mkdir(iter)
 
         ## construct roster of files
-        file_inventory: pd.DataFrame = pd.DataFrame({'year':range(2011, pd.Timestamp.now().year)})
+        file_inventory: pd.DataFrame = pd.DataFrame({'year':range(start_year, pd.Timestamp.now().year)})
         file_inventory['status'] = 'unknown'
         file_inventory['file'] = file_inventory.apply(
             lambda row: f"input/countyinflow{row['year']-2000}{row['year']-1999}.csv", axis= 1)
-        file_inventory['url'] = file_inventory.apply(
-            lambda row: f"https://www.irs.gov/pub/irs-soi/{row['file']}", axis= 1)
+        file_inventory['url'] = file_inventory['file'].str.replace(
+            pat="input/", repl="https://www.irs.gov/pub/irs-soi/")
         file_inventory= file_inventory.set_index('year')
 
         ## download files if they do not already exist
@@ -82,64 +84,85 @@ class IRS:
         assert not ('unknown' in set(file_inventory.status)), "Some file retrievals skipped"
         print('==== File retrieval outcomes ================')
         print(file_inventory['status'].value_counts().sort_index())
+
+        ## store useful objects for future functions
         self.file_inventory = file_inventory
+        self.valid_years = file_inventory.index[file_inventory['status'] != 'Not found']
         return self
     
-    #### extract data from a file
-    def extract_move_data_from_file(self, year) -> pd.DataFrame:
+
+    def extract_data_from_file(self, year) -> pd.DataFrame:
         """Extract useful information from a csv file"""
 
         ## read in useful columns
         cols= {'y1_statefips':str, 'y1_countyfips':str, 'y2_statefips':str}
         cols.update({'y2_countyfips':str, 'n2':int, 'agi':int})
-        move_data= pd.read_csv(
+        data_from_file= pd.read_csv(
             self.file_inventory.at[year, 'file'],
             usecols= cols.keys(), dtype= cols)
 
         ## filter out summary codes and improve column name interpretability
-        move_data= move_data.loc[move_data['y1_statefips'].astype(int)< 57]
-        move_data= move_data.rename(
+        data_from_file= data_from_file.loc[data_from_file['y1_statefips'].astype(int)< 57]
+        data_from_file= data_from_file.rename(
             {'n2':'people', 'y2_statefips':'dest', 'y1_statefips':'origin'}, axis= 1)
-    
+        
+        ## ensure leading zeros are included in fips codes
+        cols = {'dest':2, 'origin':2, 'y2_countyfips':3, 'y1_countyfips':3}
+        for i in cols.keys():
+            data_from_file[i] = data_from_file[i].str.zfill(cols[i])
+
         ## assemble full fips codes
-        move_data['dest']= move_data['dest'] + move_data['y2_countyfips']
-        move_data['origin']= move_data['origin'] + move_data['y1_countyfips']
-        move_data = move_data.drop(['y1_countyfips', 'y2_countyfips'], axis= 1)
-        self.move_data = move_data
+        data_from_file['dest']= data_from_file['dest'] + data_from_file['y2_countyfips']
+        data_from_file['origin']= data_from_file['origin'] + data_from_file['y1_countyfips']
+        data_from_file = data_from_file.drop(['y1_countyfips', 'y2_countyfips'], axis= 1)
+
+        ## index and store
+        data_from_file['year'] = year
+        data_from_file = data_from_file.set_index(['year', 'origin', 'dest']).sort_index()
+        self.data_from_file[year] = data_from_file
+        return self
+    
+
+    def extract_move_data(self, conserve_memory=False):
+        """ extract useful data from all valid files"""
+        if conserve_memory:
+            first_iteration = True
+            for i in self.valid_years:
+                self.extract_data_from_file(i)
+                if first_iteration:
+                    self.move_data = self.data_from_file[i].iloc[0:0]
+                    first_iteration = False
+                self.move_data = pd.concat(
+                    [self.move_data, self.data_from_file[i]], axis=0, join='outer')
+                del self.data_from_file[i]
+        else:
+            for i in self.valid_years:
+                self.extract_data_from_file(i)
+            self.move_data = pd.concat(self.data_from_file, axis=0, join='outer')
         return self
 
 
-
-########## REFINE IRS DATA
-
-
-
-########## DEFINE CLASS FOR IRS DATA (OR LEAVE AS A DATAFRAME?)
+########## PULL COUNTY COORDINATES, ADD DISTANCES TO IRS DATA OBJECT & COORDS TO CENSUS
+## Perhaps rename as edge and node data?
 
 
 ########## PULL COUNTY CHARACTERISTICS DATA FROM BEA (OR CENSUS IF THAT'S BETTER)
-
 
 
 ########## TEST EXECUTION OF ALL CODE
 
 if __name__ == '__main__':
 
-    ## initate a parallel processing pool
-    pool= multiprocessing.Pool(processes= min(max(os.cpu_count() // 2, 1), 8))
+    ## time execution
+    start_time = pd.Timestamp.now()
 
-    ## download irs data (if not already downloaded)
+    ## retrieve irs county to county movement data
     irs_data = IRS().make_file_inventory()
-    irs_data.extract_move_data_from_file(2011)
-    print(dir(irs_data))
+    irs_data.extract_move_data()
+    print(irs_data.move_data)
 
-    ## process and compile data
-    #irs_data= pool.map_async(
-    #    func= process_irs_data,
-    #    callback= lambda x:x.head(5), #TODO: construct class
-    #    iterable= 
-    #)
-    #print(irs_data)
-
+    ## print execution time
+    print('Execution time:', pd.Timestamp.now() - start_time)
+    
 
 ##########==========##########==========##########==========##########==========##########==========
