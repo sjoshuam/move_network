745d8283a164337c6e14410402dc44a880425a02
sjoshuam <sjoshuam@github.com>
2025-09-11 12:03:07 -0400

    Make setup script specific to OS and elaborate contingencies

--- setup.sh
+++ setup.sh
@@ -1,10 +1,33 @@
 #!/bin/bash
 
+## Abort if not using the designated operating system 
+if [ ! $(grep ^VERSION_ID=  /etc/os-release) == 'VERSION_ID="24.04"' ]; then
+  echo 'ERROR: Script designed to run on Ubuntu 24.04; aborting...'
+  exit 1
+fi
+
+if [ ! $(grep ^NAME=  /etc/os-release) == 'NAME="Ubuntu"' ]; then
+  echo 'ERROR: Script designed to run on Ubuntu 24.04; aborting...'
+  exit 1
+fi
+
 # update operating system
 if [ $(uname -s) == 'Linux' ]; then
   sudo DEBIAN_FRONTEND=noninteractive apt-get update -y #qq
   sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -y #qq
+  sudo DEBIAN_FRONTEND=noninteractive apt-get autoremove -y #qq
 fi
+
+# Update python; abort if 3.12 isn't linked to python3
+sudo apt-get install python3.12
+sudo apt-get install python3-pip
+sudo apt-get install python3-venv
+
+if ! python3 -V 2>&1 | grep -q "^Python 3\.12"; then
+  echo "ERROR: Script designed to run with Python 3.12"
+  exit 2
+fi
+
 
 # build and activate virtual environment
 python3.12 -m venv env
